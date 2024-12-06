4584b352a256ad082ff9f59dc5d87f4f01ddc59f
harshitbhardwaj-09 <harshitbhardwaj99999@gmail.com>
2024-12-06 17:43:01 +0530

    ..

--- controllers/project.controller.js
+++ controllers/project.controller.js
@@ -120,14 +120,18 @@ export const getProjects = async (req, res) => {
 };
 
 export const getProjectById = async (req, res) => {
+    try {
         const { id } = req.params;
         const project = await Project.findById(id);
         if (!project) {
-            return res.status(404).json({ message: "Project not found" });        
+            return res.status(404).json({ message: "Project not found" });
         }
-        res.status(200).json({project} );
+        res.status(200).json({ project });
+    } catch (error){
+        console.error('Error fetching project:', error);
+        res.status(500).json({ message: "Server error", error });
+    }
 };
-
 
 export const getAllTasksByProjectId = async (req, res) => {
     try {
