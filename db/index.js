9ba4a08001d2a414e9f432bd7625e37c24abf385
harshitbhardwaj-09 <harshitbhardwaj99999@gmail.com>
2024-12-06 16:24:08 +0530

    deployment

--- db/index.js
+++ db/index.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
 
+dotenv.config()
 
 const connectDB = async () => {
     try {
+        // console.log(process.env.MONGODB_URI)
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-// import dotenv from "dotenv"
-// import connectDB from "./db/index.js";
-// import express from "express"
-// import cors from "cors"
-// import cookieParser from "cookie-parser"
-// import multer from "multer";
-// import rateLimit from "express-rate-limit";
-// //import createserver from "http"
-// //import {Server} from "socket.io"
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import multer from "multer";
+import rateLimit from "express-rate-limit";
+import authRouter from './routes/auth.js'
+import apiRouter from './routes/api.js'
+connectDB()
+dotenv.config()
+//import createserver from "http"
+//import {Server} from "socket.io"
 
-// const app = express()
-// //const upload = multer(); // Initialize multer without any storage configuration for handling form-data
-
-
-
-// app.use(cors({
-//     origin: process.env.CORS_ORIGIN,
-//     credentials: true
-// }))
-
-// app.use(express.json({limit: "16kb"}))
-// app.use(express.urlencoded({extended: false, limit: "16kb"}))
-// app.use(express.static("public"))
-// app.use(cookieParser())
+const app = express()
+//const upload = multer(); // Initialize multer without any storage configuration for handling form-data
 
 
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
-// import authRouter from './routes/auth.js'
-// import apiRouter from './routes/api.js'
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: false, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
 // const limiter = rateLimit({
 //     windowMs: 15 * 60 * 1000, // 15 minutes
 //     max: 100, // Limit each IP to 100 requests per `window` (15 minutes)
@@ -38,30 +37,27 @@
 //     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 // });
 
-// // Apply the rate limiter to all requests
+// Apply the rate limiter to all requests
 // app.use(limiter);
-// app.use("/admin",authRouter);
-// app.use("/api",apiRouter);
-// app.get("/", (req, res) => {
-//     res.send("Server is live")
-// })
+app.get("/", (req, res) => {
+    res.send("Server is live")
+})
+app.use("/admin",authRouter);
+app.use("/api",apiRouter);
 
-// // const server = createserver(app); // Create the HTTP server using Express app
-// // const io = new Server(server, {
-// //   cors: {
-// //     origin: process.env.CORS_ORIGIN,
-// //     methods: ["GET", "POST"],
-// //     credentials: true,
-// //   },
-// // });
+// const server = createserver(app); // Create the HTTP server using Express app
+// const io = new Server(server, {
+//   cors: {
+//     origin: process.env.CORS_ORIGIN,
+//     methods: ["GET", "POST"],
+//     credentials: true,
+//   },
+// });
 
-// dotenv.config({
-//     path: './.env'
-// })
 
-// app.listen(process.env.PORT || 8000, () => {
-//     console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-// })
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+})
 
 // connectDB()
 // .then(() => {
@@ -72,15 +68,3 @@
 // })
 
 
-
-import express from "express"
-const app = express()
-
-app.get("/", (req, res) => {
-    res.send("Server is live")
-})
-
-
-app.listen(process.env.PORT || 8005, () => {
-    console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-})
